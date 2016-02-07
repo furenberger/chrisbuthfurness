f149dbc0a82e51a6a9b32a5ab50595a92e26895b
furenberger <furenberger@gmail.com>
2016-02-06 19:25:03 -0600

    initial version

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2,
+          sourceMap: true,
+          sourceMapFilename: "less/creative.css.map",
+          sourceMapBasepath: "less/"
+        },
+        files: {
+          "less/creative.css": "less/creative.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
---
+++ exhibitions.js
@@ -0,0 +1,23 @@
+{
+    "exhibitions": [{
+        "title": "California Water Media",
+        "active" : true,
+        "date": "January 9 through February 7, 2016",
+        "text" : "Sebastopol Center for the Arts, Sebastopol, CA"
+    }, {
+        "title": "Retrospective - Viewing Spaces: Drawn, Painted and Installed",
+        "active" : true,
+        "date": "July 23, 2015 through October 8, 2015",
+        "text" : "Places/Spaces: Drawn, Painted, Installed <br> This retrospective exhibition of over 30 works from 1980 to the present includes watercolor paintings from several series - Sky in a Box, Empty Spaces and California. In addition, graphite drawings, pastels and one editioned intaglio print are displayed as well as small studies for larger works. All work is sold with its frame an can be delivered."
+    }, {
+        "title": "HUDSON Business Lounge",
+        "active" : true,
+        "date": "",
+        "text" : "310 East Buffalo<br>Milwaukee, WI 53202 <br> The Hudson Business Lounge and Cafe is open: <br> Monday thru Thursday 8am-9pm <br>  Friday 8am-10pm Saturday 10am-10pm <br>  Sunday closed"
+    }, {
+        "title": "Represented at Purloin Studios",
+        "active" : true,
+        "date": "",
+        "text" : "N88 W16567 Main Street <br>Menomonee Falls, WI"
+    }]
+}
