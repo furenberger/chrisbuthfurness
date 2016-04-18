7265a40904c40ce76e9ba5b0d4f9623b57e03691
furenberger <furenberger@gmail.com>
2016-04-18 08:03:25 -0500

    minor style changes

--- js/json/exhibitions.js
+++ js/json/exhibitions.js
@@ -1,32 +1,51 @@
-{
-    "exhibitions": [{
+{ "exhibitions" : [
+    {
+        "title": "",
+        "url" : "",
+        "urlTitle" : "",
+        "active" : false,
+        "date": "",
+        "text" : ""
+    },
+    {
+        "title": "Exhibiting in Watercolor USA 2016",
+        "url" : "http://www.sgfmuseum.org/182/Watercolor-USA-2016",
+        "urlTitle" : "Watercolor USA 2016",
+        "active" : true,
+        "date": "June 4 to August 28, 2016",
+        "text" : "Springfield Art Museum, Springfield, MO"
+    },
+    {
         "title": "California Water Media",
-        "url" : "http://www.purloinstudio.com/index.html",
-        "urlTitle" : "Purloin Studios",
+        "url" : "http://sebarts.org/",
+        "urlTitle" : "Sebastopol Center for the Arts",
         "active" : true,
         "date": "January 9 through February 7, 2016",
         "text" : "Sebastopol Center for the Arts, Sebastopol, CA"
-    }, {
+    },
+    {
         "title": "Retrospective - Viewing Spaces: Drawn, Painted and Installed",
-        "url" : "http://www.purloinstudio.com/index.html",
-        "urlTitle" : "Purloin Studios",
+        "url" : "",
+        "urlTitle" : "",
         "active" : true,
         "date": "July 23, 2015 through October 8, 2015",
         "text" : "Places/Spaces: Drawn, Painted, Installed <br> This retrospective exhibition of over 30 works from 1980 to the present includes watercolor paintings from several series - Sky in a Box, Empty Spaces and California. In addition, graphite drawings, pastels and one editioned intaglio print are displayed as well as small studies for larger works. All work is sold with its frame an can be delivered."
-    }, {
+    },
+    {
         "title": "HUDSON Business Lounge",
-        "url" : "http://www.purloinstudio.com/index.html",
-        "urlTitle" : "Purloin Studios",
+        "url" : "http://www.hudson-business-lounge.com/",
+        "urlTitle" : "HUDSON Business Lounge",
         "active" : true,
         "date": "",
         "text" : "310 East Buffalo<br>Milwaukee, WI 53202 <br> The Hudson Business Lounge and Cafe is open: <br> Monday thru Thursday 8am-9pm <br>  Friday 8am-10pm Saturday 10am-10pm <br>  Sunday closed"
-    }, {
+    },
+    {
         "title": "Represented at Purloin Studios",
         "url" : "http://www.purloinstudio.com/index.html",
         "urlTitle" : "Purloin Studios",
         "active" : true,
         "date": "",
         "text" : "N88 W16567 Main Street <br>Menomonee Falls, WI"
-    }]
-}
+    }
+]}
 
