d1e9b7aa03308c9c38d16290d53e2d685ad9d43c
Ryan Furness <ryanfurness@northwesternmutual.com>
2017-11-10 12:04:10 -0600

    adding MASTER environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "ENVIRONMENT FTP $1"
+ENVIRONMENT=$1
+echo "ENVIRONMENT FTP ${ENVIRONMENT}"
+
+MASTER="MASTER"
+
 
 echo $USER
 
@@ -8,6 +12,9 @@ echo $USER
 cd /home/travis/build/furenberger/chrisbuthfurness/build
 
 # FTP the files in the build folder to the environment you want
+if [ ${ENVIRONMENT} != ${MASTER} ]; then
+# RUN THE NON MASTER DEPLOY
+echo "RUNNING NOT MASTER"
 ftp -invp ${HOST} <<EOF
 user ${USER} ${PASSWORD}
 cd test
@@ -15,6 +22,19 @@ pwd
 mput *
 bye
 EOF
+
+else
+# RUN THE MASTER DEPLOY
+echo "RUNNING MASTER"
+ftp -invp ${HOST} <<EOF
+user ${USER} ${PASSWORD}
+cd test
+pwd
+mput *
+bye
+EOF
+
+fi
 
 # Exit the script
 exit 0
